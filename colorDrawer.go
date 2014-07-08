286bce8b371ae760d3fec58480b0d81af3bdb54d
Øyvind Nordsveen <oyvind.nordsveen@sol.no>
2014-07-08 15:03:29 +0200

    Moved image saving and creation to worSurface

--- colorDrawer.go
+++ colorDrawer.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"github.com/nordsoyv/colorDrawer/config"
-	"github.com/nordsoyv/colorDrawer/imageutil"
-//	"github.com/nordsoyv/colorDrawer/colorCube"
+	//	"github.com/nordsoyv/colorDrawer/colorCube"
 	"fmt"
-	"image"
+	"github.com/nordsoyv/colorDrawer/workSurface"
 )
-
-
 
 func main() {
 
 	configuration := config.Read("config.json")
 
-//	cube := colorCube.New(uint8(configuration.ColorCubeBitSize))
+	//	cube := colorCube.New(uint8(configuration.ColorCubeBitSize))
 
-	imageSize := 1 << uint( ( (configuration.ColorCubeBitSize + configuration.ColorCubeBitSize + configuration.ColorCubeBitSize) / 2))
+	imageSize := 1 << uint(( (configuration.ColorCubeBitSize + configuration.ColorCubeBitSize + configuration.ColorCubeBitSize) / 2))
 	fmt.Println("imageSize : ", imageSize)
-	img := imageutil.CreateRandomImage(imageSize)
-	imageutil.WriteImageToDisk(configuration.OutputFilename, img)
+	//	img := imageutil.CreateRandomImage(imageSize)
+	//	imageutil.WriteImageToDisk(configuration.OutputFilename, img)
+
+	work := workSurface.New(imageSize)
+
+	work.FillWithRandomColors()
+	work.ToPng(configuration.OutputFilename)
+
+
 
 }
 
--- workSurface/workSurface.go
+++ workSurface/workSurface.go
@@ -1,6 +1,14 @@
 package workSurface
 
-import "image/color"
+import (
+	"image/color"
+	"math/rand"
+	"os"
+	"bufio"
+	"image/png"
+	"image"
+	"fmt"
+)
 
 type pixel struct {
 	Color color.RGBA
@@ -12,12 +20,12 @@ type Surface struct{
 	Size   int
 }
 
-func New(size int) Surface {
-	topLevel := make([][] pixel, size)
+func New(sideSize int) Surface {
+	topLevel := make([][] pixel, sideSize)
 	for i := range topLevel {
-		topLevel[i] = make([]pixel, size)
+		topLevel[i] = make([]pixel, sideSize)
 	}
-	return Surface{topLevel, size}
+	return Surface{topLevel, sideSize}
 }
 
 func (s *Surface) GetColor(x, y int) color.RGBA {
@@ -29,9 +37,9 @@ func (s *Surface) GetColor(x, y int) color.RGBA {
 
 func (s *Surface) SetColor(x, y int, r, g, b uint8) {
 	if x >= s.Size || y >= s.Size {
-		panic("SetColor :: index out of range")
+		panic(fmt.Sprintf("SetColor :: index out of range, was %v or %v, should be max %v", x, y, s.Size))
 	}
-	s.pixels[x][y].Color = color.RGBA{r,g,b,255}
+	s.pixels[x][y].Color = color.RGBA{r, g, b, 255}
 
 }
 
@@ -48,4 +56,48 @@ func (s *Surface) IsUsed(x, y int) bool {
 		panic("IsUsed :: index out of range")
 	}
 	return s.pixels[x][y].Used
+}
+
+func (s *Surface) FillWithRandomColors() {
+	for x := 0; x < s.Size; x++ {
+		for y := 0 ; y < s.Size; y++ {
+			R := uint8(rand.Intn(256))
+			G := uint8(rand.Intn(256))
+			B := uint8(rand.Intn(256))
+			s.SetColor(x, y, R, G, B)
+			s.SetUsed(x, y)
+		}
+	}
+}
+
+func randomColor() (color.RGBA) {
+	R := uint8(rand.Intn(256))
+	G := uint8(rand.Intn(256))
+	B := uint8(rand.Intn(256))
+	return color.RGBA{R, G, B, 255}
+}
+
+func (s *Surface) ToPng(fileName string) {
+	outfile , err := os.Create(fileName)
+	check(err)
+	defer outfile.Close()
+
+	writer := bufio.NewWriter(outfile)
+	png.Encode(writer, s.toImage())
+}
+
+func (s *Surface) toImage() *image.RGBA {
+	img := image.NewRGBA(image.Rect(0, 0, s.Size, s.Size))
+	for x := 0; x < s.Size; x++ {
+		for y := 0 ; y < s.Size; y++ {
+			img.Set(x, y, s.GetColor(x, y))
+		}
+	}
+	return img
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
